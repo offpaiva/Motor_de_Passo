f399b4cd08d1b950626438c9684da378f083bc17
Lucas5397 <78931636+Lucas5397@users.noreply.github.com>
2021-05-21 16:01:56 -0300

    Add files via upload

---
+++ MotordePassos.X/delay.c
@@ -0,0 +1,21 @@
+/*
+ * File:   delay.c
+ * Author: 20185315
+ *
+ * Created on 29 de Abril de 2021, 13:33
+ */
+
+
+#include <xc.h>
+#include "delay.h"
+
+void delay( unsigned int t )
+{
+    while( t )
+    {
+        __delay_ms( 1 );
+        --t;
+    }
+}
+
+   
---
+++ MotordePassos.X/main.c
@@ -0,0 +1,25 @@
+/*
+ * File:   main.c
+ * Author: 20185315
+ *
+ * Created on 21 de Maio de 2021, 14:37
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+
+#define PORTDbits.RD2   A-    
+#define PORTDbits.RD3   B-    
+#define PORTDbits.RD4   A+    
+#define PORTDbits.RD5   B+    
+
+void main(void)
+{
+    while(1)
+    {
+        A+ = 1;
+        delay(1000);
+    }
+}
