643a9e13a3c42937c5d7532e182836e67a77c35a
20185206 <20185206@DSN1271158627.cfp127aluno.local>
2021-06-04 13:32:49 -0300

    motor final somativa

--- MotordePassos.X/main.c
+++ MotordePassos.X/main.c
@@ -14,16 +14,21 @@
 #define cw 1
 #define ccw -1
 
+// inserir no while as funçoes (stepMotor, meiopasso, passo)
+// cw= sentido horario cw= antihorario
+// colocar primeiro sentido, depois o grau, depois o tempo
+// usar mais de uma funçao requer o uso do delay
+
 void main (void)
 {
     stepMotor_init (8);
     
     while( 1 )
     {
-        stepMotor(ccw, 360,20);
+        meiopasso(cw, 360,20);
         delay(300);
-        stepMotor(cw, 360,20);
-    }    delay(3000);
+        stepMotor(ccw, 90, 20);
+    }    
 }
 
 
